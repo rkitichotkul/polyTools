96a9e40fca3053c33c90febd16d4e73fa516e48c
Ruangrawee Kitichotkul <ruangrawee7@DN2lk5k84.stanford.edu>
2019-07-10 11:38:05 -0700

    Add getEnergies

--- polyTools.py
+++ polyTools.py
@@ -11,7 +11,6 @@ import scipy.signal as sgl
 
 # In[ ]:
 
-
 # savePoints: list(start, end, skip) inclusive
 def r2vt(n: int, savePoints: list, directory: str):
     numSavePoints = (savePoints[1] - savePoints[0]) // savePoints[2] + 1
@@ -31,7 +30,7 @@ def r2Atn(n: int, savePoints: list, directory: str):
     numBeads = getNumBeads(directory)
     numSegPerChain = (numBeads - 1) // n
     numSavePoints = (savePoints[1] - savePoints[0]) // savePoints[2] + 1
-    
+
     result = np.array([])
     for i in range(numSavePoints):
         fname = directory + 'r' + str(i * savePoints[2]+ savePoints[0]) + 'v0'
@@ -61,7 +60,7 @@ def rLAtn(n: int, savePoints: list, l0: float, directory: str):
     chainLength = l0 * n
     numSegPerChain = (numBeads - 1) // n
     numSavePoints = (savePoints[1] - savePoints[0]) // savePoints[2] + 1
-    
+
     result = np.array([])
     for i in range(numSavePoints):
         fname = directory + 'r' + str(i * savePoints[2]+ savePoints[0]) + 'v0'
@@ -76,23 +75,23 @@ def rLAtn(n: int, savePoints: list, l0: float, directory: str):
 
 def rLAtnVt(n: int, savePoints: list, duration: int, l0: float, directory: str):
     numDurations = (savePoints[1] - savePoints[0]) // duration
-    
+
     durOut = np.array([[]])
     result = np.array([[]])
     for i in range(numDurations):
-        rLAtThisDur = rLAtn(n, (i * duration, (i + 1) * duration, savePoints[2]), l0, directory)
-        rLAtThisDur = np.array([rLAtThisDur])      
+        rLAtThisDur = rLAtn(n, (i * duration + savePoints[0], (i + 1) * duration + savePoints[0], savePoints[2]), l0, directory)
+        rLAtThisDur = np.array([rLAtThisDur])
         # Keep track of the duration of each rLAtn distribution
-        thisDur = np.array([i * duration, (i + 1) * duration])
+        thisDur = np.array([i * duration + savePoints[0], (i + 1) * duration + savePoints[0]])
         thisDur = np.array([thisDur])
-        
+
         if i == 0:
             result = np.array(rLAtThisDur)
             durOut = np.array(thisDur)
-        else:           
+        else:
             result = np.append(result, rLAtThisDur, axis=0)
             durOut = np.append(durOut, thisDur, axis=0)
-           
+
     return {'durations' : durOut, 'result' : result}
 
 
@@ -114,7 +113,7 @@ def rLAtnVtHist(result: dict, numBins: int, directory: str=''):
 # In[ ]:
 
 
-def rLAtnVtHist2(result: dict, numBins: int, directory: str=''):
+def rLAtnVtHist2(result: dict, numBins: int):
     numDurations = result['result'].shape[0]
     for i in range(numDurations):
         plt.hist(result['result'][i], numBins, density=True, histtype='step', label='save points' + str(result['durations'][i]))
@@ -131,7 +130,7 @@ def uuAtn(n: int, savePoints: list, directory: str):
     numBeads = getNumBeads(directory)
     numSegPerChain = (numBeads - 1) // n
     numSavePoints = (savePoints[1] - savePoints[0]) // savePoints[2] + 1
-    
+
     result = np.array([])
     for i in range(numSavePoints):
         fname = directory + 'u' + str(i * savePoints[2]+ savePoints[0]) + 'v0'
@@ -155,9 +154,14 @@ def uuVn(nRange: list, savePoints: list, directory: str):
 
 # In[ ]:
 
-
 def getNumBeads(directory: str):
     fname = directory + 'r0v0'
     thisR = np.loadtxt(fname)
     return thisR.shape[0]
 
+
+def getEnergies(columns: list, directory: str):
+    data = np.loadtxt(directory + 'energiesv0', skiprows=1)
+    columns = np.append(0, columns)
+    result = np.array([data[:, i] for i in columns])
+    return result
